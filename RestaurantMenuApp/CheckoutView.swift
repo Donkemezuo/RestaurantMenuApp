59f67719196a8bdfdcf5d44a6bdd34711f6968f0
Donkemezuo <raymonddonkemezuo.org>
2019-10-04 14:41:28 -0400

    User can place order

--- RestaurantMenuApp/CheckoutView.swift
+++ RestaurantMenuApp/CheckoutView.swift
@@ -12,10 +12,19 @@ struct CheckoutView: View {
     @EnvironmentObject var order: Order
     
     static let paymentTypes = ["Cash", "Credit Card", "Customer Points"]
+    static let tipAmounts = [10,15,20,25,0]
+
     @State private var paymentType = 0
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
-    
+    @State private var tipAmount = 1
+    @State private var showingPaymentAlert = false
+    var totalPrice: Double {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(Self.tipAmounts[tipAmount])
+        return total + tipValue
+    }
+   
     
     var body: some View {
         Form {
@@ -35,7 +44,25 @@ struct CheckoutView: View {
                     }
                     
                 }
+            
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(0..<Self.tipAmounts.count) {
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+                
+            }
+            
+            Section(header: Text("TOTAL: $\(totalPrice, specifier: "%.2f")")) {
+                Button("Confirm order") {
+                    self.showingPaymentAlert.toggle()
+                }
+            }
         }
         .navigationBarTitle(Text("Payment"), displayMode: .inline)
+        .alert(isPresented: $showingPaymentAlert) { // This creates an alert controller
+            Alert(title: Text("Order Confirmed"), message: Text("Your total was $\(totalPrice, specifier: "%.2f") - thank you!"), dismissButton: .default(Text("Ok"))) // This is the alert action
+        }
     }
 }
